8f8fefaeec76f51f306e81e1e15ff0afe40f0825
Ruslan Akberov <ruslanakberov@gmail.com>
2024-05-31 00:37:06 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ruslan Akberov on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
